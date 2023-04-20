63dc3720c0b59df68335186ccc45762eefff8597
Oskar-droid <federov92@gmail.com>
2023-04-21 01:41:00 +0300

    table_added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import List from './Component';
 import './Style.css';
+import Table from './Table';
 
 function App() {
   return (
-    
+
+    <>
       <List />
+      <Table />
+    </>
 
   );
 }
